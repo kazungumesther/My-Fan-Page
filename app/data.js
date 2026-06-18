6a14c490d00cc3fe58db7e6234f2ddd454440a3c
Student <kazungumesther@gmail.com>
2026-06-18 21:30:11 +0300

    My finished fan page

---
+++ app/data.js
@@ -0,0 +1,99 @@
+export const items = [
+  {
+    id: 1,
+    name: "Etsy",
+    blurb: "Command Every room.",
+    rating: 5,
+    emoji: "",
+    image:"/10th.jpg"
+  },
+  {
+    id: 2,
+    name: "karla",
+    blurb: "Extreme height with architectual flair.",
+    rating: 5,
+    emoji: "",
+    image:"/Pasted image.png"
+  },
+  {
+    id: 3,
+    name: "pearl",
+    blurb: "Elegance meets all day comfort.",
+    rating: 5,
+    emoji: "",
+    image:"/second shoe.jpg"
+  },
+  {
+    id: 4,
+    name: "Kaey",
+    blurb: "Couture inspired.",
+    rating: 5,
+    emoji: "",
+    image:"/8th.jpg"
+  },
+  {
+    id: 5,
+    name: "YSL",
+    blurb: "Fashoin-Forward.",
+    rating: 5,
+    emoji: "",
+    image:"/15th.jpg"
+  },
+  {
+    id: 6,
+    name: "Salena",
+    blurb: "Bold.",
+    rating: 5,
+    emoji: "",
+    image:"/6th.jpg"
+  },
+
+   {
+    id: 7,
+    name: "Luxe",
+    blurb: "Quiet Luxury.",
+    rating: 5,
+    emoji: "",
+    image:"/17th.jpg"
+  },
+   {
+    id: 8,
+    name: "Cassey",
+    blurb: "Unapologetic glamour.",
+    rating: 5,
+    emoji: "",
+    image:"/4th.jpg"
+  },
+  {
+    id: 9,
+    name: "Ivory",
+    blurb:"Ethereal.",
+    rating: 4,
+    emoji: "",
+    image:"/16th.jpg"
+  },
+  {
+    id: 10,
+    name: "Marcela",
+    blurb: "Ultimate elegance.",
+    rating: 5,
+    emoji: "",
+    image:"/11th.jpg"
+  },
+  {
+    id: 11,
+    name: "Amardi",
+    blurb: "Glamour.",
+    rating: 5,
+    emoji: "",
+    image:"/14th.jpg"
+  },
+  {
+    id: 12,
+    name: "Burkae",
+    blurb: "Confidence in every step.",
+    rating: 5,
+    emoji: "",
+    image:"/12th.jpg"
+  },
+];
